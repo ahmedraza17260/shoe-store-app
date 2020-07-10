482806f67928282fe05f5eed672641960d5cab1d
Ahmed Raza <ahmeraza17260@gmail.com>
2020-07-10 18:23:57 +0500

    chamges in footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   return (
 	<div>
 		<Routes />
+		<div className="copyRight">
+            <h4> Powered by Ahmed Raza </h4>
+            <a href="https://github.com/ahmedraza17260" rel="noreferrer">
+              {" "}
+              <h3> Copyright &copy; 2020 Ahmed Raza </h3>{" "}
+              <h3> All Right Reserved </h3>
+            </a>
+          </div>
 	</div>
   );
 }
